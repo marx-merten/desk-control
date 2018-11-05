3ee93249cdeb2b49cf9ff03bbe92499e8081ce29
Stefan Marx <stefan@marx-merten.de>
2018-11-05 18:38:20 +0100

    Code Cleanup and TSLINT

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,9 @@
-import { DeskStack, SimpleDeskPage } from "./streamdesk/deskstack";
+import { DeskStack } from "./streamdesk/deskstack";
+import { SimpleDeskPage } from "./streamdesk/SimpleDeskPage";
 
 const deck = new DeskStack();
 
 deck.addPage(new SimpleDeskPage("MAIN"));
 deck.setMainPage("MAIN");
+
+console.log("READY Press ctrl+c to stop operations !!!");
