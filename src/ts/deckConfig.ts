64dacf41962781476050070d713f3cd45bfe1213
Stefan Marx <stefan@marx-merten.de>
2018-11-15 00:05:59 +0100

    Enabled Clor and StackState Widget

--- src/ts/deckConfig.ts
+++ src/ts/deckConfig.ts
@@ -2,5 +2,5 @@ export class DeckConfig {
   public static width = 5;
   public static height = 3;
   public static numberOfButtons = DeckConfig.width * DeckConfig.height;
-  public static clickMinimumTimeMs = 6;
+  public static clickMinimumTimeMs = 10;
 }
