2fae0b89236a0175a18765cfe29a4f07d7fe4d60
Stefan Marx <stefan@marx-merten.de>
2018-11-16 09:37:58 +0100

    Enabled Submenue

---
+++ src/ts/streamdeck/page/submenueDeckPage.ts
@@ -0,0 +1,20 @@
+import { DeckStack, KEY_CLICK } from "../deckStack";
+import { StreamKeyWrapper } from "../deckWrapper";
+import { ICON_EXIT } from "./logos";
+import { SimpleButton } from "./simpleDeckButton";
+import { SimpleDeckPage } from "./simpleDeckPage";
+import { IconLabel } from "./svgLabel";
+
+export class SubMenu extends SimpleDeckPage {
+  public stack: DeckStack;
+  private exitButton: SimpleButton;
+  constructor(name: string, stack: DeckStack) {
+    super(name);
+    this.stack = stack;
+    this.exitButton = new SimpleButton("exit", new IconLabel(ICON_EXIT, "exit"));
+    this.exitButton.on(KEY_CLICK, (key: StreamKeyWrapper) => {
+      this.stack.returnToPrevious();
+    });
+    this.addButton(this.exitButton, { x: 4, y: 2 });
+  }
+}
