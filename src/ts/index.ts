751b18ef73714e049a5166e5d9a7838391010f0f
Stefan Marx <stefan@marx-merten.de>
2018-11-16 12:10:48 +0100

    Added Icons

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,7 +2,6 @@
 import { Color, get as colorGet, to as colorTo } from "color-string";
 import { DeckStack, KEY_CLICK } from "./streamdeck/deckStack";
 import { StreamKeyWrapper } from "./streamdeck/deckWrapper";
-import { ICON_CONFIG } from "./streamdeck/page/logos";
 import { SimpleButton } from "./streamdeck/page/simpleDeckButton";
 import { SimpleDeckPage } from "./streamdeck/page/simpleDeckPage";
 import { ColorLabel, RandomColorLabel, StateSwitchLabel } from "./streamdeck/page/simpleLabels";
@@ -11,19 +10,20 @@ import { DataUtil } from "./utility";
 
 import * as fs from "fs";
 import * as path from "path";
+import { ICONS } from "./streamdeck/page/logos";
 import { SubMenu } from "./streamdeck/page/submenueDeckPage";
 const deck = new DeckStack();
 
 const subPage = new SubMenu("SUB", deck);
 for (let i = 0; i < 12; i++) {
-  subPage.addButton(new SimpleButton("nope", new IconLabel(ICON_CONFIG, "ICON:" + i)));
+  subPage.addButton(new SimpleButton("nope", new IconLabel(ICONS.ALARM, "ICON:" + i)));
 }
 
 deck.addPage(subPage);
 
 const mainPage = new SimpleDeckPage("MAIN");
 mainPage
-  .addButton(new SimpleButton("open", new IconLabel(ICON_CONFIG, "config")))
+  .addButton(new SimpleButton("open", new IconLabel(ICONS.FF, "config")))
   .on(KEY_CLICK, (key: StreamKeyWrapper) => {
     deck.jumpPage("SUB");
   });
