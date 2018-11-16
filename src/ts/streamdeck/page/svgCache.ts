42f58917f05fbd2331c53f2a6f5aeaa1f56a7cea
Stefan Marx <stefan@marx-merten.de>
2018-11-16 10:14:51 +0100

    Bugfix, missed cach file

---
+++ src/ts/streamdeck/page/svgCache.ts
@@ -0,0 +1,18 @@
+import { Md5 } from "ts-md5/dist/md5";
+
+export class SVGCache {
+  public cache: Map<string, any> = new Map();
+
+  public isCached(svgSrc: string): any {
+    const key = Md5.hashStr(svgSrc).toString();
+    return this.cache.has(key);
+  }
+  public retrieve(svgSrc: string): any {
+    const key = Md5.hashStr(svgSrc).toString();
+    return this.cache.get(key);
+  }
+  public cacheImage(src: string, img: any): void {
+    const key = Md5.hashStr(src).toString();
+    this.cache.set(key, img);
+  }
+}
